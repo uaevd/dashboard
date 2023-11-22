5697dcd0b8513ead7b18d262d4bc099706804eb5
uaevd <saekkigae45@gmail.com>
2023-11-22 21:29:47 +0700

    Add default `alt` tag in `<Image />`

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -25,6 +25,7 @@ export default async function InvoicesTable({
                                         <div className="mb-2 flex items-center">
                                             <Image
                                                 src={invoice.image_url}
+                                                alt="Image About Invoice"
                                                 className="mr-2 rounded-full"
                                                 width={28}
                                                 height={28}
@@ -83,6 +84,7 @@ export default async function InvoicesTable({
                                         <div className="flex items-center gap-3">
                                             <Image
                                                 src={invoice.image_url}
+                                                alt="Image About Invoice"
                                                 className="rounded-full"
                                                 width={28}
                                                 height={28}
